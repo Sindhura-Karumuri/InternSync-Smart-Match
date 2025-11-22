6c78a997221770ae4b63064ae4e8f2f763303766
Sindhura-Karumuri <karu07sind@gmail.com>
2025-11-22 18:58:15 +0530

    Update frontend API URL to deployed backend

--- pm-internship-prototype2/frontend/src/utils/api.js
+++ pm-internship-prototype2/frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://internsync-smart-match.onrender.com",
 });
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem("token");
